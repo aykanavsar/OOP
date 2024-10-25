ce254220463274fc8bc9174bc5b1d362cbf199da
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-10-25 11:22:57 +0300

    4.3

---
+++ Pratik 4.3 Kutuphane/Program.cs
@@ -0,0 +1,17 @@
+﻿using Pratik_4._3_Kutuphane;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        kitap kitap1 = new kitap();
+        kitap1.BookName = "Adı Aylin";
+        kitap1.ArtName = "Ayşe";
+        kitap1.ArtSurname = "Kulin";
+        kitap1.PageCount = "398";
+        kitap1.Publisher = "Remzi Kitapevi";
+
+        kitap1.Info();
+
+    }
+}
---
+++ Pratik 4.3 Kutuphane/kitap.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_4._3_Kutuphane
+{
+    internal class kitap
+    {
+        public string _bookName;
+        public string _artName;
+        public string _artSurname;
+        public string _pageCount;
+        public string _publisher;
+        public DateTime _recordDate;
+
+        public string BookName
+        {
+            get
+            {
+                return _bookName;
+            }
+
+            set
+            {
+                _bookName = value;
+            }
+
+        }
+
+        public string ArtName
+        {
+            get
+            {
+                return _artName;
+            }
+
+            set
+            {
+                _artName = value;
+            }
+
+        }
+
+        public string ArtSurname
+        {
+            get
+            {
+                return _artSurname;
+            }
+
+            set
+            {
+                _artSurname = value;
+            }
+
+        }
+
+        public string PageCount
+        {
+            get
+            {
+                return _pageCount;
+            }
+
+            set
+            {
+                _pageCount = value;
+            }
+        }
+
+        public string Publisher
+        {
+            get
+            {
+                return _publisher;
+            }
+
+            set
+            {
+                _publisher = value;
+            }
+        }
+
+        public kitap()
+        {
+            _bookName = "Girilmedi";
+            _artName = "Girilmedi";
+            _artSurname = "Girilmedi";
+            _pageCount = "Girilmedi";
+            _publisher = "Girilmedi";
+            _recordDate = DateTime.Now; 
+
+
+        }
+
+        public kitap(string bookName, string artName, string artSurname, string pageCount, string publisher)
+        {
+            _bookName = bookName;
+            _artName = artName;
+            _artSurname=artSurname;
+            _pageCount = pageCount; 
+            _publisher = publisher; 
+            _recordDate = DateTime.Now;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Kitap Adı : {_bookName}\nYazar Adı Soyadı  : {_artName} {_artSurname}\nSayfa Sayısı : {_pageCount}\nYayın Evi : {_publisher}\nKayıt Tarihi : {_recordDate}");
+            Console.WriteLine(new String('_', 33));
+        }
+
+
+    }
+}
