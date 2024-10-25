bb723dcbdd52d256415bb88d8f7d41f18ced3eaa
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-10-25 10:33:45 +0300

    4.2.

---
+++ Pratik 4.2 Constructor/Baby.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_4._2_Constructor
+{
+    internal class Baby
+    {
+        public string _name;
+        public string _surname;
+        public DateTime _birth;
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                _name = value;
+            }
+        }
+            public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+
+            set
+            {
+                _surname = value;
+            }
+
+        }
+        public DateTime Birth
+        {
+            get
+            {
+                return _birth;
+            }
+
+            set
+            {
+                _birth = value;
+            }
+        }
+
+        public Baby()
+        {
+            _birth = DateTime.Now;
+            FirstCry();
+        }
+
+        public Baby(string name, string surname)
+        {
+            _name =name;
+            _surname = surname;
+            _birth = DateTime.Now;
+            FirstCry();
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Hoşgeldin {_name} Bebek ");
+            Console.WriteLine($"Adı : {_name}\nSoyadı : {_surname}\nDoğum Tarihi : {_birth}");
+        }
+
+        public void FirstCry()
+        {
+            Console.WriteLine("Ingaaaaaaa");
+        }
+
+    
+            
+
+    
+    }
+}
---
+++ Pratik 4.2 Constructor/Program.cs
@@ -0,0 +1,23 @@
+﻿using Pratik_4._2_Constructor;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Baby Bebek1 = new Baby();
+
+        Bebek1.Name = "Ahmet Ege";
+        Bebek1.Surname = "Avşar";
+        Bebek1.Info();
+
+        Console.WriteLine(new string('-', 30));
+
+        Baby Bebek2 = new Baby();
+        Bebek2.Info();
+
+
+
+        Console.ReadKey();
+
+    }
+}
