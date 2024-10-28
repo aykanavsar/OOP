57fa87ea286d1f6a93c21623191b1701446f19df
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-10-28 15:33:11 +0300

    Geometri

---
+++ Polymorphism/BaseGeometrikSekil.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public abstract class BaseGeometrikSekil
+    {
+        public int genislik {  get; set; }
+        public int yukseklik { get; set; }
+        public string sekil { get; set; }
+        
+
+
+        public void Print()
+        {
+            Console.WriteLine($"Genişlik : {genislik}\nYükseklik : {yukseklik}\nOlan {sekil} Alanı : {AlanHesapla()}");
+        }
+
+        public virtual double AlanHesapla()
+        {
+            return genislik * yukseklik;
+        }
+
+    }
+
+    public class Kare : BaseGeometrikSekil
+    {
+        public Kare()
+        {
+            sekil = "Kare";
+        }
+    }
+    public class Dikdortgen : BaseGeometrikSekil
+    {
+        public Dikdortgen()
+        {
+            sekil = "Dikdörtgen";
+        }
+    }
+
+    public class Ucgen : BaseGeometrikSekil
+    {
+        public Ucgen()
+        {
+            sekil = "Üçgen";
+        }
+
+        public override double AlanHesapla()
+        {
+            return base.AlanHesapla() / 2;
+        }
+    }
+
+
+
+
+}
---
+++ Polymorphism/Program.cs
@@ -0,0 +1,23 @@
+﻿using Polymorphism;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Kare kare = new Kare();
+        kare.yukseklik = 4;
+        kare.genislik = 4;
+
+        Dikdortgen diktorgen = new Dikdortgen();    
+        diktorgen.genislik = 8;
+        diktorgen.yukseklik = 2;
+
+        Ucgen ucgen = new Ucgen();
+        ucgen.genislik = 9;
+        ucgen.yukseklik = 2;
+
+        kare.Print();
+        diktorgen.Print();
+        ucgen.Print();
+    }
+};
