6e3aada4f9ede5d7073d7f29123f48bb37cdc5e4
Ali Avşar <vimsagrup@VimsaGrup.onmicrosoft.com>
2024-10-28 17:52:14 +0300

    11

---
+++ Pratik 4.10 Abstraction/Program.cs
@@ -0,0 +1,23 @@
+﻿using Pratik_4._10_Abstraction;
+using System.Runtime.CompilerServices;
+
+internal class Program
+{
+
+    private static void Main(string[] args)
+    {
+        YazilimGelistirici Dev1 = new YazilimGelistirici("Aykan", "Avsar");
+        Dev1.Gorev();
+
+        ProjeYoneticisi py1 = new ProjeYoneticisi("Mahmut", "Tuncel");
+        py1.Gorev();
+
+        SatisDanismani st1 = new SatisDanismani("Ahmet", "Akar");
+        st1.Gorev();
+        
+
+        
+        Console.ReadKey();
+    }
+    
+}
---
+++ Pratik 4.10 Abstraction/Sirket.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_4._10_Abstraction
+{
+    public abstract  class Sirket
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        
+
+        public Sirket(string ad, string soyad)
+        {
+            Ad = ad;
+            Soyad = soyad;
+           
+
+        }
+
+        public abstract void Gorev();
+
+    }
+
+    public class YazilimGelistirici : Sirket
+    {
+        public YazilimGelistirici(string ad, string soyad) : base(ad, soyad)
+        {
+            
+        }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} Görevi Yazılım Gelistirici.");
+        }
+    }
+
+    public class ProjeYoneticisi : Sirket
+    {
+        public ProjeYoneticisi(string ad, string soyad) : base(ad, soyad)
+        {
+
+        }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} Görevi Proje Yöneticisi.");
+        }
+    }
+
+    public class SatisDanismani : Sirket
+    {
+        public SatisDanismani(string ad, string soyad) : base(ad, soyad)
+        {
+
+        }
+
+        public override void Gorev()
+        {
+            Console.WriteLine($"{Ad} {Soyad} Görevi Satış Danışmanı.");
+        }
+    }
+}
